bc33683c5e07cea7d9b1c0984a715f2c9952a42b
Kody Liou <kody.liou@gmail.com>
2023-06-27 21:50:23 +0800

    Fix lint problem

--- sst.config.ts
+++ sst.config.ts
@@ -1,7 +1,7 @@
 import { SSTConfig } from "sst";
-import { Api } from "./stacks/Api";
-import { Web } from "./stacks/Web";
-import { Database } from "./stacks/Database";
+import { Api } from "./stacks/Api.js";
+import { Web } from "./stacks/Web.js";
+import { Database } from "./stacks/Database.js";
 
 export default {
   config(_input) {
